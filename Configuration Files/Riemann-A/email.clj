a7f4dc782879d59aff687027337e067c048637ad
anandmohnish <manand1@hawk.iit.edu>
2017-07-03 19:06:57 -0500

    Created folders and placed initial config files.

---
+++ Configuration Files/Riemann-A/email.clj
@@ -0,0 +1,10 @@
+(ns examplecom.etc.email
+  (:require [riemann.email :refer :all]))
+
+(def email (mailer {:host "smtp.gmail.com" 
+	         :user “mohnish111@gmail.com" 
+		     :pass “ilovebunnies" 
+             :tls true 
+             :port 465 
+             :from “you@example.com"}))
+
