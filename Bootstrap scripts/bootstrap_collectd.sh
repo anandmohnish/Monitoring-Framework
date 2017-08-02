19deeae706226a1cd2490cf2125a926c28a89f28
anandmohnish <manand1@hawk.iit.edu>
2017-08-01 20:21:38 -0500

    added usage to collectd bootstrap script, included hosts file changes and made some sanity changes to script.

--- Bootstrap scripts/bootstrap_collectd.sh
+++ Bootstrap scripts/bootstrap_collectd.sh
@@ -1,6 +1,24 @@
-##Chapter 5
+#Usage below.
+#Before running, change the permission on the files.
+#Execute chmod 755 bootstrap_graphite_grafana_MC.sh
+#Run the script by typing sh -x bootstrap_graphite_grafana_MC.sh
+#Reboot the host after installation is done.
+#!/bin/bash
+#Changing the hostname
+sudo hostname graphitemc
+cp -p /etc/hostname /etc/hostname_orignal
+>/etc/hostname
+echo "hosta" >/etc/hostname
+cp -p /etc/hosts /etc/hosts_orignal
+>/etc/hosts
+echo "127.0.1.1     hosta">>/etc/hosts
+echo "172.31.40.135     riemanna">>/etc/hosts
+echo "172.31.36.247     riemannmc">>/etc/hosts
+echo "172.31.42.123     logstasha">>/etc/hosts
+echo "172.31.39.166     graphitea">>/etc/hosts
+echo "172.31.9.100      hosta">>/etc/hosts
 
-#Installing collectd on Ubuntu
+#Installing collectd
 sudo add-apt-repository ppa:collectd/collectd-5.5
 
 #Update and Install collectd
@@ -11,24 +29,10 @@ sudo apt-get -y install collectd
 #collectd -h
 
 
-###Configuring collectd
-#collectd is managed via a config file, located at 
-#/etc/collectd/collectd.conf on Ubuntu. 
-
-
-##Place our config file
-
-#On Ubuntu
-#cd /tmp
-#wget https://raw.githubusercontent.com/anandmohnish/Monitoring-Framework/master/Configuration%20Files/Collectd/config%20files/collectd.conf
-
-#sudo cp /tmp/collectd.conf /etc/collectd/collectd.conf
-
-#check and create directory - was required only on ubuntu
+#create a directory
 sudo mkdir /etc/collectd.d
 
 ##Configuring the plugins for collectd
-
 #wget the various plugins from Github
 cd /tmp
 wget https://raw.githubusercontent.com/anandmohnish/Monitoring-Framework/master/Configuration%20Files/Collectd/config%20files/collectd.conf
@@ -47,51 +51,12 @@ wget https://github.com/anandmohnish/Monitoring-Framework/blob/master/Configurat
 #sudo rm /tmp/collectd.conf
 sudo cp /tmp/*.conf /etc/collectd.d/
 
-##Writing to Riemann
-#cd /tmp
-#wget https://raw.githubusercontent.com/turnbullpress/aom-code/master/5-6/collectd/collectd.d/write_riemann.conf
-##Change hostname inside this file
-#sudo cp /tmp/write_riemann.conf /etc/collectd.d/write_riemann.conf
-
-#wget https://raw.githubusercontent.com/turnbullpress/aom-code/master/5-6/collectd/collectd.d/carbon.conf
-
-
 #Enabling and running collectd
-
-#On Ubuntu
 sudo update-rc.d collectd defaults
 sudo service collectd start
 
+
 ##Check log files
 #/var/log/collectd.log
-
 #Also check riemann log files now
 #/var/log/riemann/riemann.log
-
-#Sending our collectd events to Graphite
-#use the tagged stream - add changes in riemann.conf
-
-#...
-#(tagged "collectd" graph)
-
-#check carbon for incoming collectd metrics
-#/var/log/carbon/creates.log
-
-
-#Add some chnages to have simple metrics naming pattern
-
-#On Riemann
-#cd /etc/riemann/examplecom/etc
-#sudo wget https://raw.githubusercontent.com/turnbullpress/aom-code/master/5-6/riemann/examplecom/etc/collectd.clj
-
-#include changes in riemann.config
-
-#remove - (tagged "collectd" graph) and  add
-#(require '[examplecom.etc.collectd :refer :all])
-
-#...
-
-#(tagged "collectd"
-#(smap rewrite-service graph))
-
-
